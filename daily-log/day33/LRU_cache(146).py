49021cb669edd908771163948a13708480814cac
Tatevik Harutyunyan <32800522+tatevik-h@users.noreply.github.com>
2025-08-01 11:45:30 +0400

    Create LRU_cache(146).py

---
+++ daily-log/day33/LRU_cache(146).py
@@ -0,0 +1,87 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # map key to node
+        
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+
+    def remove(self, node):
+        prev , nxt = node.prev, node.next
+        prev.next= nxt
+        nxt.prev = prev
+
+    # insert node at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            # remove from the list and hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+## Second solution
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
+        
+        self.cache.move_to_end(key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+
+
