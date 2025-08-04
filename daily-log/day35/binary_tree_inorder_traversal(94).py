138ab1b3cf4f223fb7abdc3552e2c2934894d668
Tatevik Harutyunyan <32800522+tatevik-h@users.noreply.github.com>
2025-08-04 16:11:22 +0400

    Create binary_tree_inorder_traversal(94).py

---
+++ daily-log/day35/binary_tree_inorder_traversal(94).py
@@ -0,0 +1,47 @@
+# solution 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+            return res
+
+        inorder(root)
+        return res
+
+
+
+#solution 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+      
+        return res
+      
