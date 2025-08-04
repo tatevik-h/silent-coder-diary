5e1e25aba43a82d2a75d468bad161d38d6dfbf3c
Tatevik Harutyunyan <32800522+tatevik-h@users.noreply.github.com>
2025-08-04 16:13:04 +0400

    Create binary_tree_preorder_traversal(144).py

---
+++ daily-log/day35/binary_tree_preorder_traversal(144).py
@@ -0,0 +1,45 @@
+# solution 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def preorder(root):
+            if not root:
+                return
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+            return res
+
+        preorder(root)
+        return res
+
+# solution 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            if curr:
+                res.append(curr.val)
+                stack.append(curr.right)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+
+        return res
+      
