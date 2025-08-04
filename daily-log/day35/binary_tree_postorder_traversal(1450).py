4cf2c9235a1049f0cb191559e416b1fa66929046
Tatevik Harutyunyan <32800522+tatevik-h@users.noreply.github.com>
2025-08-04 16:14:43 +0400

    Create binary_tree_postorder_traversal(1450).py

---
+++ daily-log/day35/binary_tree_postorder_traversal(1450).py
@@ -0,0 +1,51 @@
+# solution 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def postorder(root):
+            if not root:
+                return
+            postorder(root.left)
+            postorder(root.right)
+            res.append(root.val)
+            
+            return res
+
+        postorder(root)
+        return res
+
+# solution 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = [root]
+        visit = [False]
+
+        while stack:
+            curr, v = stack.pop(), visit.pop()
+            if curr:
+                if v:
+                    res.append(curr.val)
+                else:
+                    stack.append(curr)
+                    visit.append(True)
+                    stack.append(curr.right)
+                    visit.append(False)
+                    stack.append(curr.left)
+                    visit.append(False)
+
+        return res
+      
